77c88e79c23df1ac90fd3acc0037bb2f642d3a69
dgcantero <diego.gomez-cantero@t-systems.com>
2022-05-23 07:15:16 +0200

    myslq role

---
+++ playbooks/roles/mysql/files/dump.sql
@@ -0,0 +1,6 @@
+REATE TABLE IF NOT EXISTS test (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO test(message) VALUES('Esto es una prueba 1');
+  INSERT INTO test(message) VALUES('Esto es una prueba 2');         
+  INSERT INTO test(message) VALUES('Esto es una prueba 3')
